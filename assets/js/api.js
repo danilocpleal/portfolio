4fc196602a0deb3717fd1596cc8f2183cef11180
Danilo Leal <dcpleal@gmail.com>
2024-03-13 08:21:32 -0300

    Alteração json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = "https://raw.githubusercontent.com/danilocpleal/projetos_gerais/main/Portfolio/data/profile.json";    
+    const url = "https://raw.githubusercontent.com/danilocpleal/main/Portfolio/data/profile.json";    
     const response = await fetch(url);
 
     const profileData = await response.json();
